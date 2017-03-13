f3655a2cffa3c520dad72ceeb76fbabab677c434
Andrius Sirvys <sndriuss@gmail.com>
2017-03-13 21:18:30 +0000

    Delete _email.php

--- _email.php
+++
@@ -1,8 +0,0 @@
-<?php
-function sendMyMail($fromEmail, $toEmail, $fromName, $subject, $body) {
-$headers = 'From: ' . $fromName . "\r\n" .
-'Reply-To: ' . $fromEmail . "\r\n" .
-'X-Mailer: PHP/' . phpversion();
-return mail($toEmail, $subject, $body, $headers);
-}
-?>
